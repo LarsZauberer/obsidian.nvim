0220d4dd1572820df388d52165129c8f646647e1
LarsZauberer <wasser.ian@gmail.com>
2024-09-09 12:09:45 +0200

    Revert "debug9"
    This reverts commit c0afba8d79cf4def861dee7642cdabaa92d2c05e.

--- lua/obsidian/commands/follow_link.lua
+++ lua/obsidian/commands/follow_link.lua
@@ -3,7 +3,7 @@ return function(client, data)
   local opts = {}
   local all = ""
   for k, v in pairs(data) do
-    all = all .. k .. " | "
+    all = all .. k .. ": " .. tostring(v) .. " | "
   end
   print("All: [" .. all)
   if data.args and string.len(data.args) > 0 then
